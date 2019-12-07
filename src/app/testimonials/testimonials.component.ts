f427686b7d72858223bc73452ec0fa5903b23fd7
unknown <az.mohamedayman@gmail.com>
2019-12-07 06:38:02 +0200

    start edit our team and testimonial page

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -7,6 +7,114 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestimonialsComponent implements OnInit {
 
+
+  testimonials = [
+    {
+      tesPhoto: '../../assets/images/content/testimonials-author-1-1.jpg',
+      tesSubPhoto: '../../assets/images/content/testimonials-author-1-2.jpg',
+      tesTitle: 'Porcelain Veneers',
+      tesName: 'Wilmer Stevenson,',
+      tesPosition: 'Analyst',
+      tesText: `
+        I'd been avoiding the dentist for years due to bad experiences. Then due to a gum infectio
+        I had to make an emergency appointment at Medin Dental Clinic.
+        I have been a patient ever since.
+        My dentist is very reassuring and very helpful.
+      `
+    },
+    {
+      tesPhoto: '../../assets/images/content/testimonials-author-1-1.jpg',
+      tesSubPhoto: '../../assets/images/content/testimonials-author-1-2.jpg',
+      tesTitle: 'Porcelain Veneers',
+      tesName: 'Wilmer Stevenson,',
+      tesPosition: 'Analyst',
+      tesText: `
+        I'd been avoiding the dentist for years due to bad experiences. Then due to a gum infectio
+        I had to make an emergency appointment at Medin Dental Clinic.
+        I have been a patient ever since.
+        My dentist is very reassuring and very helpful.
+      `
+    },
+    {
+      tesPhoto: '../../assets/images/content/testimonials-author-1-1.jpg',
+      tesSubPhoto: '../../assets/images/content/testimonials-author-1-2.jpg',
+      tesTitle: 'Porcelain Veneers',
+      tesName: 'Wilmer Stevenson,',
+      tesPosition: 'Analyst',
+      tesText: `
+        I'd been avoiding the dentist for years due to bad experiences. Then due to a gum infectio
+        I had to make an emergency appointment at Medin Dental Clinic.
+        I have been a patient ever since.
+        My dentist is very reassuring and very helpful.
+      `
+    },
+    {
+      tesPhoto: '../../assets/images/content/testimonials-author-1-1.jpg',
+      tesSubPhoto: '../../assets/images/content/testimonials-author-1-2.jpg',
+      tesTitle: 'Porcelain Veneers',
+      tesName: 'Wilmer Stevenson,',
+      tesPosition: 'Analyst',
+      tesText: `
+        I'd been avoiding the dentist for years due to bad experiences. Then due to a gum infectio
+        I had to make an emergency appointment at Medin Dental Clinic.
+        I have been a patient ever since.
+        My dentist is very reassuring and very helpful.
+      `
+    },
+    {
+      tesPhoto: '../../assets/images/content/testimonials-author-1-1.jpg',
+      tesSubPhoto: '../../assets/images/content/testimonials-author-1-2.jpg',
+      tesTitle: 'Porcelain Veneers',
+      tesName: 'Wilmer Stevenson,',
+      tesPosition: 'Analyst',
+      tesText: `
+        I'd been avoiding the dentist for years due to bad experiences. Then due to a gum infectio
+        I had to make an emergency appointment at Medin Dental Clinic.
+        I have been a patient ever since.
+        My dentist is very reassuring and very helpful.
+      `
+    },
+    {
+      tesPhoto: '../../assets/images/content/testimonials-author-1-1.jpg',
+      tesSubPhoto: '../../assets/images/content/testimonials-author-1-2.jpg',
+      tesTitle: 'Porcelain Veneers',
+      tesName: 'Wilmer Stevenson,',
+      tesPosition: 'Analyst',
+      tesText: `
+        I'd been avoiding the dentist for years due to bad experiences. Then due to a gum infectio
+        I had to make an emergency appointment at Medin Dental Clinic.
+        I have been a patient ever since.
+        My dentist is very reassuring and very helpful.
+      `
+    },
+    {
+      tesPhoto: '../../assets/images/content/testimonials-author-1-1.jpg',
+      tesSubPhoto: '../../assets/images/content/testimonials-author-1-2.jpg',
+      tesTitle: 'Porcelain Veneers',
+      tesName: 'Wilmer Stevenson,',
+      tesPosition: 'Analyst',
+      tesText: `
+        I'd been avoiding the dentist for years due to bad experiences. Then due to a gum infectio
+        I had to make an emergency appointment at Medin Dental Clinic.
+        I have been a patient ever since.
+        My dentist is very reassuring and very helpful.
+      `
+    },
+    {
+      tesPhoto: '../../assets/images/content/testimonials-author-1-1.jpg',
+      tesSubPhoto: '../../assets/images/content/testimonials-author-1-2.jpg',
+      tesTitle: 'Porcelain Veneers',
+      tesName: 'Wilmer Stevenson,',
+      tesPosition: 'Analyst',
+      tesText: `
+        I'd been avoiding the dentist for years due to bad experiences. Then due to a gum infectio
+        I had to make an emergency appointment at Medin Dental Clinic.
+        I have been a patient ever since.
+        My dentist is very reassuring and very helpful.
+      `
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
