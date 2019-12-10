12de8db8f623fcaee0a4c9df1592c49625e32ebb
Mohamed Ayman <az.mohamedayman12@gmail.com>
2019-12-10 17:26:24 +0200

    Edit Home Page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-
+export class AboutComponent implements OnInit, OnDestroy {
+  timer;
   constructor() { }
 
   ngOnInit() {
+    //  this.timer = setInterval(function() {
+    //   window.location.reload(true);
+    // }, 1000);
+  }
+
+
+  ngOnDestroy() {
+    // clearTimeout(this.timer);
   }
 
 }
